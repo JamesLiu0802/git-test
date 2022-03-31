533255f21b33b0a2137d8570589d0f34ecfee9d1
JamesLiu <liunaijian0802@gmail.com>
2022-03-31 18:50:26 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author JamesLiu
+ * @create 2022-03-31 18:45
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
